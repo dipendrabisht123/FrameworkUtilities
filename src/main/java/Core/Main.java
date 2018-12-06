9e26ecab3b2e4bb6afc3f95e201e99dfcd3a38ec
Dipendra Bisht <dipendra.bisht1984@gmail.com>
2018-12-06 23:28:48 +0530

    moved Main file

--- src/main/java/Core/Main.java
+++ src/main/java/Core/Main.java
@@ -1,22 +1,9 @@
 package Core;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.testng.TestNG;
-import org.testng.reporters.jq.TestNgXmlPanel;
-import org.testng.xml.XmlClass;
-import org.testng.xml.XmlSuite;
-import org.testng.xml.XmlTest;
-
-import com.codoid.products.exception.FilloException;
-
 import PageClasses.Utilities.CreateTestNGXML;
 import PageClasses.Utilities.ExcelUtilities.ReadExcel;
-import TestClasses.LogInToGoogle;
 
 public class Main {
 	
